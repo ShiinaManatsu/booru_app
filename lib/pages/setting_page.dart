4c8e4f2414d8ad739f676050651394e59c9533ef
Shiina Manatus <whitesky@live.cn>
2020-01-04 22:06:04 +0800

    Update settng page

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:floating_search_bar/ui/sliver_search_bar.dart';
 import 'package:yande_web/settings/app_settings.dart';
 
 class SettingPage extends StatefulWidget {
@@ -7,26 +8,53 @@ class SettingPage extends StatefulWidget {
 }
 
 class _SettingPageState extends State<SettingPage> {
+  var locationIsExpand = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Settings"),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back,color: Colors.black87,),
-          onPressed: ()=>Navigator.pop(context),
-        ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-                prefixText: "New location here: ",
-                helperText: AppSettings.savePath),
-            onChanged: (x) => AppSettings.savePath = x,
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverFloatingBar(
+            automaticallyImplyLeading: true,
+            title: Text("Settings"),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Padding(
+                padding: const EdgeInsets.all(12),
+                child: ExpansionPanelList(
+                  expansionCallback: (index, flag) {
+                    setState(() {
+                      locationIsExpand = !flag;
+                    });
+                  },
+                  children: <ExpansionPanel>[
+                    ExpansionPanel(
+                      canTapOnHeader: true,
+                      isExpanded: locationIsExpand,
+                      headerBuilder: (context, d) {
+                        return ListTile(
+                          title: Text("Download Location"),
+                        );
+                      },
+                      body: ListTile(
+                        title: Text("Location"),
+                        subtitle: TextField(
+                          decoration: InputDecoration(
+                              prefixText: "New location here: ",
+                              helperText: AppSettings.savePath),
+                          //onChanged: (x) => AppSettings.savePath = x,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ]),
           ),
         ],
       ),
