8bad1d451c1dd3928c2287517d144c9257f513b3
Shiina Manatus <whitesky@live.cn>
2020-01-19 19:02:48 +0800

    Change page transition

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_page_transition/flutter_page_transition.dart';
+import 'package:overlay_support/overlay_support.dart';
 import 'package:yande_web/pages/post_view_page.dart';
 import 'package:yande_web/pages/setting_page.dart';
 import 'android/notifier.dart';
@@ -48,51 +50,59 @@ class MyApp extends StatelessWidget {
     SystemChrome.setSystemUIOverlayStyle(
         SystemUiOverlayStyle(statusBarColor: Colors.transparent));
 
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        onGenerateRoute: _routes(),
-        title: 'Home',
-        theme: lightTheme);
-  }
-
-  RouteFactory _routes() {
-    return (settings) {
-      final Map<String, dynamic> arg = settings.arguments;
-      Widget screen;
-      switch (settings.name) {
-        case homePage:
-          screen = HomePage();
-          break;
-        case searchTaggedPostsPage:
-          screen = SearchTaggedPostsPage(key: arg["key"]);
-          break;
-        case postViewPage:
-          screen = PostViewPage(post: arg["post"]);
-          break;
-        case settingsPage:
-          screen = SettingPage();
-          break;
-        default:
-          return null;
-      }
-      // var route = PageRouteBuilder(
-      //   pageBuilder: (context, animation, secondaryAnimation) => screen,
-      //   transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      //     var begin = Offset(0.0, 1.0);
-      //     var end = Offset.zero;
-      //     var curve = Curves.ease;
-
-      //     var tween =
-      //         Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-      //     return SlideTransition(
-      //       position: animation.drive(tween),
-      //       child: child,
-      //     );
-      //   },
-      // );
-      //return route;
-      return MaterialPageRoute(builder: (BuildContext contex) => screen);
-    };
+    return OverlaySupport(
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          // onGenerateRoute: (settings) {
+          //   final Map<String, dynamic> arg = settings.arguments;
+          //   Widget screen;
+          //   switch (settings.name) {
+          //     case homePage:
+          //       screen = HomePage();
+          //       break;
+          //     case searchTaggedPostsPage:
+          //       screen = SearchTaggedPostsPage(key: arg["key"]);
+          //       break;
+          //     case postViewPage:
+          //       screen = PostViewPage(post: arg["post"]);
+          //       break;
+          //     case settingsPage:
+          //       screen = SettingPage();
+          //       break;
+          //     default:
+          //       return null;
+          //   }
+          //   return MaterialPageRoute(builder: (BuildContext contex) => screen);
+          // },
+          onGenerateRoute: (settings) => PageRouteBuilder(
+              settings: settings,
+              pageBuilder: (context, animation, secondaryAnimation) {
+                final Map<String, dynamic> arg = settings.arguments;
+                switch (settings.name) {
+                  case homePage:
+                    return HomePage();
+                    break;
+                  case searchTaggedPostsPage:
+                    return SearchTaggedPostsPage(key: arg["key"]);
+                    break;
+                  case postViewPage:
+                    return PostViewPage(post: arg["post"]);
+                    break;
+                  case settingsPage:
+                    return SettingPage();
+                    break;
+                  default:
+                    return null;
+                }
+              },
+              transitionDuration: const Duration(milliseconds: 700),
+              transitionsBuilder:
+                  (context, animation, secondaryAnimation, child) {
+                return effectMap[PageTransitionType.transferRight](
+                    Curves.ease, animation, secondaryAnimation, child);
+              }),
+          title: 'Home',
+          theme: lightTheme),
+    );
   }
 }
