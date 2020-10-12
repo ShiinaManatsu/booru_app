5a5557d470677e607b105ec62c29a08f66a053dc
ShiinaManatsu <whitesky@live.cn>
2020-10-12 15:04:48 +0800

    New style

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -22,8 +22,6 @@ class _SettingPageState extends State<SettingPage> {
   var setLimit = PublishSubject<double>();
   String savePath = "";
 
-  var expansionStatus = [false, false, false, false, false];
-
   PreviewQuality quality = AppSettings.previewQuality;
   bool _safeMode = AppSettings.safeMode;
   bool _masonryGrid = AppSettings.masonryGrid;
@@ -43,7 +41,11 @@ class _SettingPageState extends State<SettingPage> {
       });
     });
 
-    setLimit.throttleTime(Duration(milliseconds: 200)).listen((x) {
+    setLimit
+        .throttleTime(Duration(milliseconds: 200))
+        .switchMap((value) async* {
+      yield value;
+    }).listen((x) {
       AppSettings.setPostLimit(x);
     });
   }
@@ -62,185 +64,162 @@ class _SettingPageState extends State<SettingPage> {
           SliverFloatingBar(
             backgroundColor: Theme.of(context).backgroundColor,
             automaticallyImplyLeading: true,
-            title: Text("${language.content.settings}"),
+            title: Text("${language.content.settings}",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6
+                    .copyWith(fontSize: 28)),
             leading: IconButton(
-              icon: Icon(Icons.arrow_back),
+              icon: Icon(
+                Icons.arrow_back,
+                color: Theme.of(context).textTheme.headline6.color,
+              ),
               onPressed: () => Navigator.pop(context),
             ),
           ),
-          SliverList(
-            delegate: SliverChildListDelegate([
-              Padding(
-                padding: const EdgeInsets.only(top: 12),
-                child: ExpansionPanelList(
-                  expansionCallback: (index, flag) {
-                    setState(() {
-                      expansionStatus[index] = !flag;
-                    });
-                  },
-                  children: <ExpansionPanel>[
-                    ExpansionPanel(
-                      canTapOnHeader: true,
-                      isExpanded: expansionStatus[0],
-                      headerBuilder: (context, d) {
-                        return ListTile(
-                          title: Text(
-                              "${language.content.download} ${language.content.location}"),
-                        );
-                      },
-                      body: ListTile(
-                        trailing: FlatButton(
-                          child: Text(
-                              "${language.content.select} ${language.content.folder}"),
-                          onPressed: () async {
-                            var status = await Permission.storage.status;
-                            if (status == PermissionStatus.granted) {
-                              String path = await FilesystemPicker.open(
-                                title: 'Save to folder',
-                                context: context,
-                                rootDirectory: Directory.fromUri(
-                                    Uri.directory("storage/emulated/0/")),
-                                fsType: FilesystemType.folder,
-                                pickText: 'Save file to this folder',
-                                folderIconColor: Colors.pink,
-                              );
-                              AppSettings.setSavePath(path);
-                              AppSettings.savePath.then((value) {
-                                setState(() {
-                                  savePath = value;
-                                });
-                              });
-                            } else {
-                              Permission.storage.request();
-                            }
-                          },
-                        ),
-                        title: Text("$savePath",
-                            style: Theme.of(context).textTheme.button),
-                      ),
-                    ),
-                    ExpansionPanel(
-                      canTapOnHeader: true,
-                      isExpanded: expansionStatus[1],
-                      headerBuilder: (context, d) {
-                        return ListTile(
-                          title: Text(
-                              "${language.content.singlePagePostLoadLimit}"),
-                        );
-                      },
-                      body: ListTile(
-                        title: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.max,
-                          children: <Widget>[
-                            Text(
-                                "${language.content.currentLimit}: ${postLimit.toInt()}"),
-                            Slider(
-                              value: postLimit,
-                              onChanged: (value) {
-                                setState(() {
-                                  postLimit = value;
-                                  setLimit.add(value);
-                                });
-                              },
-                              min: 40,
-                              max: 100,
-                              divisions: 60,
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                    ExpansionPanel(
-                      canTapOnHeader: true,
-                      isExpanded: expansionStatus[2],
-                      headerBuilder: (context, d) {
-                        return ListTile(
-                          title: Text(
-                              "${language.content.preview} ${language.content.quality}"),
-                        );
-                      },
-                      body: ListTile(
-                          title: DropdownButton<PreviewQuality>(
-                        items: PreviewQuality.values
-                            .map((e) => DropdownMenuItem(
-                                  value: e,
-                                  child: Text(EnumToString.parse(e)),
-                                ))
-                            .toList(),
-                        value: quality,
-                        onChanged: (value) {
-                          setState(() {
-                            quality = value;
-                            AppSettings.previewQuality = value;
-                            SharedPreferencesExtension.setTyped(
-                                "PreviewQuality", EnumToString.parse(value));
-                          });
-                        },
-                      )),
-                    ),
-                    ExpansionPanel(
-                      canTapOnHeader: true,
-                      isExpanded: expansionStatus[3],
-                      headerBuilder: (context, d) {
-                        return ListTile(
-                          title: Text(
-                              "${language.content.safe} ${language.content.mode}"),
-                        );
-                      },
-                      body: ListTile(
-                          title: Row(
-                        children: [
-                          Text(
-                              "${language.content.safe} ${language.content.mode}"),
-                          Switch(
-                            value: _safeMode,
-                            onChanged: (value) {
-                              SharedPreferencesExtension.setTyped<bool>(
-                                  "safemode", value);
-                              AppSettings.safeMode = value;
-                              setState(() {
-                                _safeMode = value;
-                              });
-                            },
-                          )
-                        ],
-                      )),
-                    ),
-                    ExpansionPanel(
-                      canTapOnHeader: true,
-                      isExpanded: expansionStatus[4],
-                      headerBuilder: (context, d) {
-                        return ListTile(
-                          title: Text("Masonry Grid"),
-                        );
-                      },
-                      body: ListTile(
-                          title: Row(
-                        children: [
-                          Text("Use Masonry Grid"),
-                          Switch(
-                            value: _masonryGrid,
-                            onChanged: (value) {
-                              SharedPreferencesExtension.setTyped<bool>(
-                                  "masonryGrid", value);
-                              AppSettings.masonryGrid = value;
-                              setState(() {
-                                _masonryGrid = value;
-                              });
-                            },
-                          )
-                        ],
-                      )),
-                    )
-                  ],
-                ),
-              ),
-            ]),
-          ),
+          //  Download location
+          _buildCard(
+              child: ListTile(
+            title: Text(
+                "${language.content.download} ${language.content.location}"),
+            subtitle:
+                Text("$savePath", style: Theme.of(context).textTheme.button),
+            isThreeLine: true,
+            trailing: Icon(Icons.folder),
+            onTap: () async {
+              var status = await Permission.storage.status;
+              if (status == PermissionStatus.granted) {
+                String path = await FilesystemPicker.open(
+                  title: 'Save to folder',
+                  context: context,
+                  rootDirectory:
+                      Directory.fromUri(Uri.directory("storage/emulated/0/")),
+                  fsType: FilesystemType.folder,
+                  pickText: 'Save file to this folder',
+                  folderIconColor: Colors.pink,
+                );
+                AppSettings.setSavePath(path);
+                AppSettings.savePath.then((value) {
+                  setState(() {
+                    savePath = value;
+                  });
+                });
+              } else {
+                Permission.storage.request();
+              }
+            },
+          )),
+          //  Load limit
+          _buildCard(
+              child: ListTile(
+            contentPadding: EdgeInsets.all(16),
+            title: Text("${language.content.singlePagePostLoadLimit}"),
+            subtitle: Row(
+              children: <Widget>[
+                Text("${language.content.currentLimit}: ${postLimit.toInt()}"),
+                Expanded(
+                  child: Slider(
+                    value: postLimit,
+                    activeColor: Theme.of(context).primaryColor,
+                    inactiveColor:
+                        Theme.of(context).primaryColor.withAlpha(0x80),
+                    onChanged: (value) {
+                      setState(() {
+                        postLimit = value;
+                        setLimit.add(value);
+                      });
+                    },
+                    min: 40,
+                    max: 100,
+                    divisions: 60,
+                  ),
+                )
+              ],
+            ),
+          )),
+          //  Preview quality
+          _buildCard(
+              child: ListTile(
+            title:
+                Text("${language.content.preview} ${language.content.quality}"),
+            trailing: DropdownButton<PreviewQuality>(
+              items: PreviewQuality.values
+                  .map((e) => DropdownMenuItem(
+                        value: e,
+                        child: Text(EnumToString.convertToString(e)),
+                      ))
+                  .toList(),
+              value: quality,
+              onChanged: (value) {
+                setState(() {
+                  quality = value;
+                  AppSettings.previewQuality = value;
+                  SharedPreferencesExtension.setTyped(
+                      "PreviewQuality", EnumToString.convertToString(value));
+                });
+              },
+            ),
+          )),
+          //  Safe mode
+          _buildCard(
+              child: ListTile(
+            title: Text("${language.content.safe} ${language.content.mode}"),
+            trailing: Switch(
+              value: _safeMode,
+              activeColor: Theme.of(context).primaryColor,
+              onChanged: (value) {
+                SharedPreferencesExtension.setTyped<bool>("safemode", value);
+                AppSettings.safeMode = value;
+                setState(() {
+                  _safeMode = value;
+                });
+              },
+            ),
+            onTap: () {
+              SharedPreferencesExtension.setTyped<bool>(
+                  "safemode", !AppSettings.safeMode);
+              AppSettings.safeMode = !AppSettings.safeMode;
+              setState(() {
+                _safeMode = !AppSettings.safeMode;
+              });
+            },
+          )),
+          //  Masonry Grid
+          _buildCard(
+              child: ListTile(
+            title: Text("Masonry Grid"),
+            trailing: Switch(
+              value: _masonryGrid,
+              activeColor: Theme.of(context).primaryColor,
+              onChanged: (value) {
+                SharedPreferencesExtension.setTyped<bool>("masonryGrid", value);
+                AppSettings.masonryGrid = value;
+                setState(() {
+                  _masonryGrid = value;
+                });
+              },
+            ),
+            onTap: () {
+              SharedPreferencesExtension.setTyped<bool>(
+                  "masonryGrid", !AppSettings.masonryGrid);
+              AppSettings.masonryGrid = !AppSettings.masonryGrid;
+              setState(() {
+                _masonryGrid = AppSettings.masonryGrid;
+              });
+            },
+          )),
         ],
       ),
     );
+  }
+
+  SliverToBoxAdapter _buildCard({Widget child}) {
+    return SliverToBoxAdapter(
+        child: Card(
+      elevation: 4,
+      margin: EdgeInsets.fromLTRB(8, 8, 8, 0),
+      child: child,
+    ));
   }
 }
 
